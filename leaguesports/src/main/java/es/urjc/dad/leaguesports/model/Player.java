510d182ea7165c93d1e0c8c5e3f01ebd72fe070e
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2022-02-08 17:37:00 +0100

    Modelo de datos de jugadores y equipos
    Creadas las clases que definen el modelo de datos de los jugadores y equipos. Un equipo tiene una lista de jugadores y un jugador puede pertenecer o no a un equipo

---
+++ leaguesports/src/main/java/es/urjc/dad/leaguesports/model/Player.java
@@ -0,0 +1,117 @@
+package es.urjc.dad.leaguesports.model;
+
+//#region external dependences
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+//#endregion
+
+@Entity
+public class Player {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String nickName;
+
+    @Column
+    private String genre;
+
+    @Column
+    private int age;
+
+    @Column
+    private String nationality;
+
+    @Column
+    @ManyToOne
+    private Team team;
+
+    public Player(long id, String firstName, String lastName, String nickName, String genre, int age, String nationality) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.genre = genre;
+        this.age = age;
+        this.nationality = nationality;
+    }
+
+    protected Player() {
+    }
+
+    //#region Getters & Setters
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }    
+    //#endregion
+
+    @Override
+    public String toString() {
+        return "Player [" + nickName + " (" + firstName + " " + lastName + ")]";
+    }
+}
---
+++ leaguesports/src/main/java/es/urjc/dad/leaguesports/model/Team.java
@@ -0,0 +1,78 @@
+package es.urjc.dad.leaguesports.model;
+
+//#region External dependences
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+//#endregion
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    @Column
+    private String teamName;
+
+    @Column
+    private int years;
+
+    @Column
+    @OneToMany(mappedBy =  "team")
+    private List<Player> players;
+
+    public Team(long id, String teamName, int years, List<Player> players) {
+        Id = id;
+        this.teamName = teamName;
+        this.years = years;
+        this.players = players;
+    }
+
+    protected Team() {
+    }
+
+    //#region Getters & Setters
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+    //#endregion
+
+    @Override
+    public String toString() {
+        return "Team [" + teamName + "]";
+    }
+}
