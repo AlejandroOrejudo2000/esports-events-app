6d77e7fb1263708894133345621e6edb73b9fd0b
RafaelCastillo2000 <61199591+RafaelCastillo2000@users.noreply.github.com>
2022-02-22 14:28:05 +0100

    Controlador imagenes

--- leaguesports/src/main/java/es/urjc/dad/leaguesports/control/MainController.java
+++ leaguesports/src/main/java/es/urjc/dad/leaguesports/control/MainController.java
@@ -1,6 +1,15 @@
 package es.urjc.dad.leaguesports.control;
 
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +19,47 @@ public class MainController {
 	public String loadMain(){
 		return "main";
 	}
+	
+private static final Path IMAGE_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
+	
+	@GetMapping("/image/lol.jpg")
+	public ResponseEntity<Object> downloadLol(Model model) throws MalformedURLException {
+		
+		Path imagePath = IMAGE_FOLDER.resolve("lol.jpg");
+		Resource image = new UrlResource(imagePath.toUri());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "lol/jpg").body(image);
+	}
+	
+	@GetMapping("/image/teams.png")
+	public ResponseEntity<Object> downloadTeams(Model model) throws MalformedURLException {
+		
+		Path imagePath = IMAGE_FOLDER.resolve("teams.png");
+		Resource image = new UrlResource(imagePath.toUri());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "teams/png").body(image);
+	}
+	
+	@GetMapping("/image/tournaments.png")
+	public ResponseEntity<Object> downloadTournaments(Model model) throws MalformedURLException {
+		
+		Path imagePath = IMAGE_FOLDER.resolve("tournaments.png");
+		Resource image = new UrlResource(imagePath.toUri());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "tournaments/png").body(image);
+	}
+	
+	@GetMapping("/image/players.png")
+	public ResponseEntity<Object> downloadPlayers(Model model) throws MalformedURLException {
+		
+		Path imagePath = IMAGE_FOLDER.resolve("players.png");
+		Resource image = new UrlResource(imagePath.toUri());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "players/png").body(image);
+	}
+	
+	@GetMapping("/image/products.png")
+	public ResponseEntity<Object> downloadProducts(Model model) throws MalformedURLException {
+		
+		Path imagePath = IMAGE_FOLDER.resolve("products.png");
+		Resource image = new UrlResource(imagePath.toUri());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "products/png").body(image);
+	}
+	
 }
