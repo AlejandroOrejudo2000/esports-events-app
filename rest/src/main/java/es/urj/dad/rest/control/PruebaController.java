13c897fe23bbcced32a4f241ea80381ccbde917c
Yago Navarrete <yago2000xd@gmail.com>
2022-03-15 19:17:09 +0100

    Correo API Rest
    Servicio de envío de emails desde la aplicación Rest.
    
    Se tiene que conectar con la aplicación web principal para enviar el correo a la dirección de cada usuario de la base de datos.

--- rest/src/main/java/es/urj/dad/rest/control/PruebaController.java
+++ rest/src/main/java/es/urj/dad/rest/control/PruebaController.java
@@ -1,13 +1,33 @@
 package es.urj.dad.rest.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PruebaController {
-    
-    @GetMapping("/prueba")
+	
+	@Autowired
+    private JavaMailSender javaMailSender;
+	
+    @GetMapping("/email")
     public Prueba prueba() {
+    	sendEmail();
         return new Prueba("A", 1);
     }
+    
+    public void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("hecoyek688@shopxda.com");
+
+        msg.setSubject("Prueba correo Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
+    
 }
