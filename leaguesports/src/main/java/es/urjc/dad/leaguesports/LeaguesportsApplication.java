51914a35bb9869557e5a4f7c00642b7f40efafe4
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2022-05-02 16:36:30 +0200

    configuracion de hazelcast

--- leaguesports/src/main/java/es/urjc/dad/leaguesports/LeaguesportsApplication.java
+++ leaguesports/src/main/java/es/urjc/dad/leaguesports/LeaguesportsApplication.java
@@ -1,13 +1,35 @@
 package es.urjc.dad.leaguesports;
 
+import java.util.Collections;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
+@EnableHazelcastHttpSession
+@EnableCaching
 @SpringBootApplication
 public class LeaguesportsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeaguesportsApplication.class, args);
+	}
+
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true)
+			.setMembers(Collections.singletonList("127.0.0.1"));
+
+		return config;
 	}
 
 }
